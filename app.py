2b1501a8be9fb3a13f719b3753090002494e86a4
Priyank Trivedi <priyankt68@gmail.com>
2016-04-11 17:33:24 +0530

    adding urls for about and projects page

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route('/about')
+def about():
+    return 'The about page'
 
 @app.route('/')
 def hello():
     return "Hello World!"
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}".format(name)
 
 if __name__ == '__main__':
     app.run()
